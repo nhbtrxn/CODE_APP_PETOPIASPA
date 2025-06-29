5c8f8ce4f1e7f18a589329ed297e8d93ef8f9e32
tran <baotranxx612@gmail.com>
2025-06-29 16:04:40 +0700

    add homepage

--- app/src/main/java/com/nhom9/adapters/SliderAdapter.java
+++ app/src/main/java/com/nhom9/adapters/SliderAdapter.java
@@ -1,4 +1,49 @@
 package com.nhom9.adapters;
 
-public class SliderAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nhom9.myapplication.R;
+
+public class SliderAdapter extends RecyclerView.Adapter<SliderAdapter.SliderViewHolder> {
+
+    Context context;
+    int[] images;
+
+    public SliderAdapter(Context context, int[] images) {
+        this.context = context;
+        this.images = images;
+    }
+
+    @NonNull
+    @Override
+    public SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_slider, parent, false);
+        return new SliderViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SliderViewHolder holder, int position) {
+        holder.imageButton.setImageResource(images[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class SliderViewHolder extends RecyclerView.ViewHolder {
+        ImageButton imageButton;
+
+        public SliderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageButton = itemView.findViewById(R.id.imageButton);
+        }
+    }
 }
--- app/src/main/java/com/nhom9/myapplication/MainActivity.java
+++ app/src/main/java/com/nhom9/myapplication/MainActivity.java
@@ -8,12 +8,33 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.nhom9.adapters.SliderAdapter;
+import com.nhom9.myapplication.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    int[] imageList = {
+            R.drawable.connect,
+            R.drawable.forum,
+            R.drawable.blog
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+
+        SliderAdapter adapter = new SliderAdapter(this, imageList);
+        binding.viewPager.setAdapter(adapter);
+
+//        // Gắn tab indicator
+//        new TabLayoutMediator(binding.tabIndicator, binding.viewPager,
+//                (tab, position) -> {
+//                }
+//        ).attach();
 
     }
 }
